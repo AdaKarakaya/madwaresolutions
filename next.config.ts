d1daed99bdc5c1a531d586aff20761b78a048536
Ada Haydar Karakaya <adakarakaya@Ada-MacBook-Air.local>
2026-02-03 17:31:23 +0300

    Remove basePath/assetPrefix for custom domain

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/madwaresolutions' : '',
   trailingSlash: true,
 };
 
